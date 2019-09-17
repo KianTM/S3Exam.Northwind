1f1da4f56857c1b50e4684e47e2c4834177afa43
KianTM <kian0949@edu.campusvejle.dk>
2019-09-17 09:10:32 +0200

    Add project files.

---
+++ S3Exam.Northwind.DAL/Repository.cs
@@ -0,0 +1,31 @@
+﻿using S3Exam.Northwind.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3Exam.Northwind.DAL
+{
+    public class Repository
+    {
+        private NorthwindModel model = new NorthwindModel();
+
+        public List<Employee> GetAllEmployees()
+            => model.Employees.ToList();
+
+        public void Update(Employee employee)
+        {
+            model.Employees.AddOrUpdate(employee);
+            model.SaveChanges();
+        }
+
+        public void Insert(Employee employee)
+        {
+            model.Employees.Add(employee);
+            model.SaveChanges();
+        }
+    }
+}
---
+++ S3Exam.Northwind.Gui/StackedButtonNavigationUserControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace S3Exam.Northwind.Gui
+{
+    /// <summary>
+    /// Interaction logic for StackedButtonNavigationUserControl.xaml
+    /// </summary>
+    public partial class StackedButtonNavigationUserControl : UserControl
+    {
+        MainWindow mainWindow;
+        private List<ToggleButton> buttons;
+
+        public StackedButtonNavigationUserControl(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+            buttons = new List<ToggleButton>();
+            buttons.Add(hrButton);
+            buttons.Add(ordersButton);
+            buttons.Add(productsButton);
+            buttons.Add(earningsButton);
+            buttons.Add(customerButton);
+            buttons.Add(providerButton);
+        }
+
+        private void HrButton_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (var button in buttons)
+            {
+                if (button.IsChecked == true && button.Name != "hrButton")
+                {
+                    button.IsChecked = false;
+                }
+            }
+            mainWindow.DetailsUserControl.Content = new HRUserControl();
+        }
+    }
+}
